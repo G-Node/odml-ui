e6650db44356a55bb50ab9c5c40ff938d55ed709
digaru19 <digsblogger@gmail.com>
2017-07-07 16:24:12 +0530

    Separate 'gi' dependencies check

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,13 +10,11 @@ python3 --version
 sudo apt-get -qq update
 sudo apt-get install libffi-dev libglib2.0-0 libglib2.0-dev
 sudo apt-get install gobject-introspection
-sudo apt-get install python-gi python3-gi
-sudo apt-get install gir1.2-glib-2.0 
+sudo apt-get install python-gi python3-gi 
+sudo apt-get install libgtk-3-dev gir1.2-glib-2.0 
 
-python -c "import gi"
-python -c "from gi import pygtkcompat"
-python3 -c "import gi"
-python3 -c "from gi import pygtkcompat"
+python test/gi_imports.py
+python3 test/gi_imports.py
 
 echo -e "\n\n #########################################"
 echo -e " Installed all dependencies successfully !!"

6f5e961de346bf38f4c45b6e061d5d6159e2e9d9
digaru19 <digsblogger@gmail.com>
2017-08-18 16:05:27 +0530

    Disable test for existence of images
    In setuptools, the generated egg file contains the images, and it is okay to disable this check.

--- test/check_resources.py
+++ test/check_resources.py
@@ -1,23 +1,23 @@
 
-import os
-import sys
-
-prefix = sys.prefix
-
-pixmaps = prefix + '/share/pixmaps/'
-applications = prefix + '/share/applications/'
-
-img_files = os.listdir(os.path.abspath("images") )
-
-# Check if image files exist in the system.
-for i in img_files:
-    if not os.path.isfile(pixmaps + i):
-        print("Not found :- %s" % pixmaps + i)
-        exit(1)
-
-if not os.path.isfile(applications + 'odml.desktop'):
-    print("Not found :- %s" % (applications + 'odml.desktop'))
-    exit(1)
+# import os
+# import sys
+# 
+# prefix = sys.prefix
+# 
+# pixmaps = prefix + '/share/pixmaps/'
+# applications = prefix + '/share/applications/'
+# 
+# img_files = os.listdir(os.path.abspath("images") )
+# 
+# # Check if image files exist in the system.
+# for i in img_files:
+#     if not os.path.isfile(pixmaps + i):
+#         print("Not found :- %s" % pixmaps + i)
+#         exit(1)
+# 
+# if not os.path.isfile(applications + 'odml.desktop'):
+#     print("Not found :- %s" % (applications + 'odml.desktop'))
+#     exit(1)
 
 ### If importing 'gi' works in a virtual environment, we can
 ### use the following method to check if 'Gtk' dependencies

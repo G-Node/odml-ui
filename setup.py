9e80246f71de336d7dd2ff1c4fd4360599f4c8a0
digaru19 <digsblogger@gmail.com>
2017-08-18 14:02:14 +0530

    Update dependencies in README and setup.py for odML-ui v1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ import sys
 import os
 import glob
 
-from distutils.core import setup
+# Use setuptools compulsarily, as the distutils doen't work out well for the 
+# installation procedure. The 'install_requires' and 'data_files' have better
+# support in setuptools.
+from setuptools import setup
+
 
 try:
     # only necessary for the windows build
@@ -19,13 +23,16 @@ packages = [
     'odmlui.treemodel'
 ]
 
+install_req = ["odml==1.3.*"]
+
 data_files = [('share/applications', ['odml.desktop']),
               ('share/pixmaps', glob.glob(os.path.join("images", "*")))
               ]
 
+
 setup(name='odML-UI',
       version='1.3',
-      description='open metadata Markup Language',
+      description='odML Editor',
       author='Hagen Fritsch',
       author_email='fritsch+gnode@in.tum.de',
       url='http://www.g-node.org/projects/odml',
@@ -36,6 +43,7 @@ setup(name='odML-UI',
               'includes': 'cairo, pango, pangocairo, atk, gobject, gio, lxml, gzip, enum34',
           }
       },
+      install_requires=install_req,
       scripts=['odml-gui'],
       data_files=data_files
       )

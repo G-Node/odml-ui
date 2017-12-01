defcba5955c9a0ddfcfe92e008bc3b56d525d147
M. Sonntag <michael.p.sonntag@gmail.com>
2017-12-01 17:00:07 +0100

    [info] Add info file for version etc

---
+++ odmlui/info.py
@@ -0,0 +1,8 @@
+VERSION = '1.4.dev'
+STATUS = 'Release'
+RELEASE = '%s %s' % (VERSION, STATUS)
+AUTHOR = 'Shubham Dighe, Hagen Fritsch, Christian Kellner, Jan Grewe, Achilleas Koutsou, \
+Michael Sonntag'
+COPYRIGHT = '(c) 2011-2017, German Neuroinformatics Node'
+CONTACT = 'dev@g-node.org'
+HOMEPAGE = 'https://github.com/G-Node/odml-ui'

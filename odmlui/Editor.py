3967f6f2e87ce00ce4b8383d1083528b993feea5
M. Sonntag <michael.p.sonntag@gmail.com>
2017-12-01 16:45:03 +0100

    [Editor] Use content from LICENSE file
    Closes #56

--- odmlui/Editor.py
+++ odmlui/Editor.py
@@ -84,21 +84,9 @@ ui_info = \
 </ui>'''
 
 
-license_lgpl = \
-    '''This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public License as
-published by the Free Software Foundation; either version 3 of the
-License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Library General Public License for more details.
-
-You should have received a copy of the GNU Library General Public
-License along with the Gnome Library; see the file COPYING.LIB.  If not,
-write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.\n'''
+license = ""
+with open("LICENSE") as f:
+    license = f.read()
 
 def gui_action(name, tooltip=None, stock_id=None, label=None, accelerator=None):
     """
@@ -436,7 +424,7 @@ class EditorWindow(gtk.Window):
             "Hagen Fritsch <fritsch+odml@in.tum.de>",
             ])
         dialog.set_website(self.odMLHomepage)
-        dialog.set_license (license_lgpl)
+        dialog.set_license(license)
         dialog.set_logo(logo)
 
         dialog.set_transient_for(self)

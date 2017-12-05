3c2e562226674cc096f52d962ca4d0d4849f5df3
M. Sonntag <michael.p.sonntag@gmail.com>
2017-12-05 14:43:22 +0100

    [setup] Add classifiers to info and setup

--- odmlui/info.py
+++ odmlui/info.py
@@ -6,3 +6,12 @@ Michael Sonntag'
 COPYRIGHT = '(c) 2011-2017, German Neuroinformatics Node'
 CONTACT = 'dev@g-node.org'
 HOMEPAGE = 'https://github.com/G-Node/odml-ui'
+CLASSIFIERS = [
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: BSD License',
+    'Development Status :: 4 - Beta',
+    'Topic :: Scientific/Engineering',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: End Users/Desktop'
+]

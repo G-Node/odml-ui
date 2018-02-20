0c1552913aff0257c0a1d83c021131e087311284
M. Sonntag <michael.p.sonntag@gmail.com>
2018-02-20 16:52:30 +0100

    [setup] Quick fix local install dependency issue

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 import glob
 import os
 
-from odmlui.info import VERSION, AUTHOR, CONTACT, HOMEPAGE, CLASSIFIERS
 # Use setuptools compulsorily, as the distutils doesn't work out well for the
 # installation procedure. The 'install_requires' and 'data_files' have better
 # support in setuptools.
@@ -14,6 +13,25 @@ try:
     kwargs.update({'console': ['odml-gui']})
 except ImportError:
     py2exe = None
+
+try:
+    from odml.info import AUTHOR, CONTACT, CLASSIFIERS, HOMEPAGE, VERSION
+except ImportError as ex:
+    # Read the information from odml.info.py if package dependencies
+    # are not yet available during a local install.
+    CLASSIFIERS = ""
+    with open('odml/info.py') as f:
+        for line in f:
+            curr_args = line.split(" = ")
+            if len(curr_args) == 2:
+                if curr_args[0] == "AUTHOR":
+                    AUTHOR = curr_args[1].replace('\'', '').replace('\\', '').strip()
+                elif curr_args[0] == "CONTACT":
+                    CONTACT = curr_args[1].replace('\'', '').strip()
+                elif curr_args[0] == "HOMEPAGE":
+                    HOMEPAGE = curr_args[1].replace('\'', '').strip()
+                elif curr_args[0] == "VERSION":
+                    VERSION = curr_args[1].replace('\'', '').strip()
 
 
 class PackageNotFoundError(Exception):

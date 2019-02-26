0783948f3eff7aae0db5c33d14d269b32b83c2b6
M. Sonntag <michael.p.sonntag@gmail.com>
2019-02-26 12:27:15 +0100

    [chooserDialog] Add docstrings

--- odmlui/chooser_dialog.py
+++ odmlui/chooser_dialog.py
@@ -1,3 +1,7 @@
+"""
+'chooser_dialog' contains odML specific file chooser widget classes.
+"""
+
 import pygtkcompat
 import gtk
 
@@ -6,6 +10,10 @@ pygtkcompat.enable_gtk(version='3.0')
 
 
 class ChooserDialog(gtk.FileChooserDialog):
+    """
+    The ChooserDialog class extends the gtk.FileChooserDialog class
+    to enable both opening and saving of files.
+    """
     def __init__(self, title, save):
         default_button = gtk.STOCK_SAVE if save else gtk.STOCK_OPEN
         default_action = gtk.FILE_CHOOSER_ACTION_SAVE if save else gtk.FILE_CHOOSER_ACTION_OPEN
@@ -18,6 +26,16 @@ class ChooserDialog(gtk.FileChooserDialog):
         self.connect('response', self.response)
 
     def response(self, widget, response_id):
+        """
+        The 'response' method is executed when a file has been
+        selected for opening or when a file path has been provided
+        for saving a file. Closes and destroys the Window once its done.
+
+        A set 'file_type' is only required for a 'on_accept: save file' callback.
+
+        :param widget: gtk.Widget object
+        :param response_id: gtk.RESPONSE signal
+        """
         if response_id == gtk.RESPONSE_OK:
             self.hide()
 
@@ -35,11 +53,17 @@ class ChooserDialog(gtk.FileChooserDialog):
     def on_accept(self, uri, file_type):
         """
         This method is overridden by gi introspection.
+        The actual method is set as a callback on the class.
         """
         pass
 
 
 class OdmlChooserDialog(ChooserDialog):
+    """
+    The OdmlChooserDialog class extends the custom
+    ChooserDialog class to handle file filters for
+    all supported odML specific file types.
+    """
     XML = "XML format (*.xml, *.odml)"
     YAML = "YAML format (*.yaml, *.odml)"
     JSON = "JSON format (*.json, *.odml)"
@@ -64,6 +88,12 @@ class OdmlChooserDialog(ChooserDialog):
         format_filter.add_pattern('*.odml')
 
     def xml_filter(self):
+        """
+        Creates and provides a file filter for XML files
+        and files with an '.odml' ending.
+
+        :return: gtk.FileFilter specific for XML files.
+        """
         x_fil = gtk.FileFilter()
         x_fil.set_name(self.XML)
         x_fil.add_pattern('*.xml')
@@ -71,6 +101,12 @@ class OdmlChooserDialog(ChooserDialog):
         return x_fil
 
     def yaml_filter(self):
+        """
+        Creates and provides a file filter for YAML files
+        and files with an '.odml' ending.
+
+        :return: gtk.FileFilter specific for YAML files.
+        """
         y_fil = gtk.FileFilter()
         y_fil.set_name(self.YAML)
         y_fil.add_pattern('*.yaml')
@@ -79,6 +115,12 @@ class OdmlChooserDialog(ChooserDialog):
         return y_fil
 
     def json_filter(self):
+        """
+        Creates and provides a file filter for JSON files
+        and files with an '.odml' ending.
+
+        :return: gtk.FileFilter specific for JSON files.
+        """
         j_fil = gtk.FileFilter()
         j_fil.set_name(self.JSON)
         j_fil.add_pattern('*.json')
@@ -86,6 +128,12 @@ class OdmlChooserDialog(ChooserDialog):
         return j_fil
 
     def add_filters(self):
+        """
+        Adds all available class filters to the filter selection.
+
+        If the dialog has been called with the intention to save
+        a file, the filter option "All files" is added as well.
+        """
         self.add_filter(self.xml_filter())
         self.add_filter(self.yaml_filter())
         self.add_filter(self.json_filter())

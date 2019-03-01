0765704be308ff5a118e31b6ccd597008271ff2c
M. Sonntag <michael.p.sonntag@gmail.com>
2019-03-01 16:20:14 +0100

    [tests/nav_bar] Add testfile

---
+++ test/test_navigation_bar.py
@@ -0,0 +1,14 @@
+"""
+Tests for odmlui.navigation_bar class.
+"""
+
+import unittest
+
+from odmlui.navigation_bar import NavigationBar
+
+
+class TestNavigationBar(unittest.TestCase):
+
+    def setUp(self):
+        self.nav_bar = NavigationBar()
+

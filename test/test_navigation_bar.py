511b22ea9cdb34cfb31ea172bb800a7fdfcd285b
M. Sonntag <michael.p.sonntag@gmail.com>
2019-03-01 16:22:19 +0100

    [tests/nav_bar] Add document property test

--- test/test_navigation_bar.py
+++ test/test_navigation_bar.py
@@ -24,6 +24,45 @@ class TestNavigationBar(unittest.TestCase):
         self.assertIsNone(self.nav_bar._current_object)
         self.assertEqual([], self.nav_bar._current_hierarchy)
 
+    def test_document(self):
+        self.assertIsNone(self.nav_bar.document)
+
+        doc = self.create_ui_doc("Author")
+
+        # make sure the change handler is empty
+        self.assertIsInstance(doc, odmlui.treemodel.nodes.Document)
+        self.assertIsNone(doc._change_handler)
+
+        self.nav_bar.document = doc
+
+        # check that the document has been set on the nav_bar
+        self.assertEqual(doc, self.nav_bar.document)
+
+        # make sure the proper nav_bar method has been set
+        # on the documents change handler
+        self.assertEqual(1, len(doc._change_handler))
+        self.assertEqual(doc._change_handler.handlers[0], self.nav_bar.on_section_changed)
+
+        # test that the document root is selected
+        self.assertEqual(1, len(self.nav_bar._current_hierarchy))
+        self.assertEqual(doc, self.nav_bar._current_hierarchy[0])
+        self.assertEqual(doc, self.nav_bar._current_object)
+
+        # test label text
+        self.assertEqual(self.nav_bar.get_label(),
+                         "Attributes | <a href=\"\"><b>Document</b></a> ")
+
+        # test reset with a different document
+        other_doc = self.create_ui_doc("Other author")
+        self.nav_bar.document = other_doc
+
+        self.assertNotEqual(doc, self.nav_bar.document)
+        self.assertNotEqual(doc, self.nav_bar._current_hierarchy[0])
+        self.assertNotEqual(doc, self.nav_bar._current_object)
+        self.assertEqual(other_doc, self.nav_bar.document)
+        self.assertEqual(other_doc, self.nav_bar._current_hierarchy[0])
+        self.assertEqual(other_doc, self.nav_bar._current_object)
+
     @staticmethod
     def create_ui_doc(author=""):
         doc = odml.Document(author=author)

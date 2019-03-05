c7aba689c13ae9fe6e398cf57efd66b935c5ff6b
M. Sonntag <michael.p.sonntag@gmail.com>
2019-03-05 11:10:20 +0100

    [textEditor] Add save and cancel buttons
    Closes #155

--- odmlui/text_editor.py
+++ odmlui/text_editor.py
@@ -32,29 +32,39 @@ class TextEditor(gtk.Window):
             attr = "pseudo_values"
 
         self.attr = attr
-        self.set_title("Editing %s. Closing window saves changes" % repr(obj))
+        self.set_title("Editing %s" % repr(obj))
         self.set_default_size(600, 600)
-        self.connect('destroy', self.on_close)
 
+        # Text container
         self.text = gtk.TextView()
         text_buffer = self.text.get_buffer()
         text_buffer.set_text(getattr(obj, attr))
+        text_container = ScrolledWindow(self.text)
 
-        self.add(ScrolledWindow(self.text))
+        # Main window container. Using the recommended gtk.Box with
+        # gtk.ORIENTATION_VERTICAL fails so we stick
+        # to the deprecated VBox for now.
+        vbox = gtk.VBox()
+        vbox.pack_start(text_container)
+
+        # Button Container
+        hbox = gtk.Box(gtk.ORIENTATION_HORIZONTAL)
+        save = gtk.Button("Save")
+        save.connect('clicked', self.on_ok)
+        hbox.add(save)
+
+        cancel = gtk.Button("Cancel")
+        cancel.connect('clicked', self.on_cancel)
+        hbox.add(cancel)
+
+        h_align = gtk.Alignment(1, 0, 0, 0)
+        h_align.add(hbox)
+
+        vbox.pack_start(h_align, False, False)
+
+        self.add(vbox)
+
         self.show_all()
-
-    def on_close(self, _):
-        """
-        Reads the text buffer value, and updates
-        the target object with the new value via
-        a command.ChangeValue object.
-        """
-        text_buffer = self.text.get_buffer()
-        start, end = text_buffer.get_bounds()
-        include_hidden_text = False
-        text = text_buffer.get_text(start, end, include_hidden_text)
-        cmd = ChangeValue(object=self.obj, attr=self.attr, new_value=text)
-        cmd()
 
     def on_cancel(self, _):
         """
